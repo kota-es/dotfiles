7c59e029df9c5b9ef1f0fba5eef45043c3d9673a
kota-es <info@k-3110.com>
2023-09-07 09:01:28 +0900

    feat: add setting

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,3 +8,14 @@ require("keymaps")
 require('gitsigns').setup()
 require('lualine').setup()
 require("colorizer").setup()
+
+require('telescope').setup{ 
+  defaults = { 
+    file_ignore_patterns = { 
+      "node_modules",
+      "vendor"
+    }
+  }
+}
+
+vim.g['fern#default_hidden']=1
